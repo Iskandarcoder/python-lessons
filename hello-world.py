2af1560bb16ba4a6f8340eadfea140f926565a21
Iskandar <iskandarcoder@gmail.com>
2022-01-12 15:01:44 -0500

    yangi fayl
    python dasturlash tili bo'yicha darslar

---
+++ hello-world.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#"""
+#Created on Mon Jan 10 14:20:55 2022
+
+#@author: Iskandarbek
+
+
+#ism = 'Iskandarbek'
+#print(ism.upper())
+
+avtolar = ['nexia', 'spark', 'damas', 'malibu', 'lacetti', 'captiva']
+
+for avto in avtolar:
+    if avto == 'spark':
+        print ('\n', avto.upper(), '\n')
+    else:
+        print(avto.title())
